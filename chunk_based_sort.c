d98aa636a96e9126c949254059a2b4830f4a9bfb
Haruya Kanamitsu <hkanamit@c5r4s9.42tokyo.jp>
2026-05-14 16:11:22 +0900

    ok

--- chunk_based_sort.c
+++ chunk_based_sort.c
@@ -6,27 +6,34 @@
 /*   By: hkanamit <hkanamit@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 15:32:44 by hkanamit          #+#    #+#             */
-/*   Updated: 2026/05/14 15:44:27 by hkanamit         ###   ########.fr       */
+/*   Updated: 2026/05/14 16:10:39 by hkanamit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
+int root(int lst_count)
+{
+	int r = 0;
+	while((long)(r * r) <= 2147483647)
+	{
+		if((r*r == lst_count))
+			return r;
+	}
+	return -1;
+}
 void	chunk_based_sort(t_list **a_lst,t_list **b_lst)
 {
 	t_list *lst = *a_lst;
 	int lst_count = 0;
 	int i = 0;
+	int root = 0;
 	while(lst->next != NULL)
 	{
 		lst_count++;
 		lst = lst->next;
 	}
-
-	while(i < lst_count)
-	{
-		
-	}
+	root(lst_count);
+	while()
 
 
 
