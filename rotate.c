157b7d8bfd51fabbcf4ad719a50fbc61cab5d09e
Haruya Kanamitsu <hkanamit@c5r4s9.42tokyo.jp>
2026-05-14 13:42:20 +0900

    r created

--- rotate.c
+++ rotate.c
@@ -6,7 +6,7 @@
 /*   By: hkanamit <hkanamit@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 12:36:58 by hkanamit          #+#    #+#             */
-/*   Updated: 2026/05/14 12:51:17 by hkanamit         ###   ########.fr       */
+/*   Updated: 2026/05/14 13:41:58 by hkanamit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,23 @@ void	rotate(t_list **node_ptr)
 	tmp->next = first;
 	first->next = NULL;
 	*node_ptr = second;
+}
+
+void	ra(t_list **a_node)
+{
+	rotate(a_node);
+	write(1, "ra\n", 3);
+}
+
+void	ra(t_list **b_node)
+{
+	rotate(b_node);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_list **a_node, t_list **b_node)
+{
+	rotate(a_node);
+	rotate(b_node);
+	write(1, "rr\n", 3);
 }
