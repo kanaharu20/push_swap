2e184847d2fad597a9c2ef061dedc8c2995de51e
Haruya Kanamitsu <hkanamit@c5r4s9.42tokyo.jp>
2026-05-15 12:27:53 +0900

    fixed

--- disorder.c
+++ disorder.c
@@ -6,7 +6,7 @@
 /*   By: hkanamit <hkanamit@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 11:10:55 by hkanamit          #+#    #+#             */
-/*   Updated: 2026/05/15 11:19:07 by hkanamit         ###   ########.fr       */
+/*   Updated: 2026/05/15 12:27:39 by hkanamit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	disorder(t_list **a_lst)
 {
-	int		mistakes;
-	int		total_pairs;
+	float	mistakes;
+	float	total_pairs;
 	t_list	*tmp;
 
 	mistakes = 0;
