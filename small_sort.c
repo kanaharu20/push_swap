ba58e71fb6c1266569e87b74614b0f8ed5216ac7
Haruya Kanamitsu <hkanamit@c5r4s9.42tokyo.jp>
2026-05-19 15:46:51 +0900

    ok

--- small_sort.c
+++ small_sort.c
@@ -6,18 +6,66 @@
 /*   By: hkanamit <hkanamit@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 12:20:52 by hkanamit          #+#    #+#             */
-/*   Updated: 2026/05/19 15:20:29 by hkanamit         ###   ########.fr       */
+/*   Updated: 2026/05/19 15:46:16 by hkanamit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-void small_sort(t_list *a_lst ,t_data *bench_data)
+
+void	small_sort3(t_list *a_lst, t_list *b_lst, t_data *bench_data)
 {
-	int x = a_lst->content;
-	int y = a_lst->content;
-	
+	t_list	*tmp;
+
+	tmp = a_lst;
+	while (tmp != NULL)
+	{
+		if (tmp->rank == 0 || tmp->rank == 1)
+			pa(&a_lst, &b_lst, bench_data);
+		ra(&a_lst, bench_data);
+		tmp = tmp->next;
+	}
+	if (b_lst->rank > b_lst->next->content)
+		sb(&a_lst, bench_data);
+	pb(&a_lst, &b_lst, bench_data);
+	pb(&a_lst, &b_lst, bench_data);
 }
-int	base_sort(t_list *a_lst, t_data *bench_data)
+void	small_sort2(t_list *a_lst, t_list *b_lst, t_data *bench_data)
+{
+	t_list	*tmp;
+
+	tmp = a_lst;
+	while (tmp != NULL)
+	{
+		if (a_lst->rank == 0)
+			pa(&a_lst, &b_lst, bench_data);
+		ra(&a_lst, bench_data);
+		tmp = tmp->next;
+	}
+	small_sort(a_lst, bench_data);
+	pb(&a_lst, &b_lst, bench_data);
+}
+
+void	small_sort(t_list *a_lst, t_data *bench_data)
+{
+	int	x;
+	int	y;
+	int	z;
+
+	x = a_lst->rank;
+	y = a_lst->next->rank;
+	z = a_lst->next->next->rank;
+	if (x < y && y > z)
+		ra(&a_lst, bench_data);
+	else if (x > y && y < z)
+		sa(&a_lst, bench_data);
+	else if (x < y && y > z)
+		ra(&a_lst, bench_data);
+	else if ((x > y && y < z))
+		rra(&a_lst, bench_data);
+	else if (x > y && y > z)
+		sa(&a_lst, bench_data);
+}
+int	base_sort(t_list *a_lst, t_list *b_lst, t_data *bench_data)
 {
 	int	count;
 
@@ -28,8 +76,14 @@ int	base_sort(t_list *a_lst, t_data *bench_data)
 	{
 		if (a_lst->rank > a_lst->next->content)
 			sa(&a_lst, bench_data);
-		return (1);
 	}
 	else if (count == 3)
-		small_sort()
+		small_sort(a_lst, bench_data);
+	else if (count == 4)
+		small_sort2(a_lst, b_lst, bench_data);
+	else if (count == 5)
+		small_sort3(a_lst, bench_data);
+	if (count <= 5)
+		return (1);
+	return (0);
 }
